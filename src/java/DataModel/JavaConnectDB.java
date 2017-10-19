7877a2d8dd49f21fa2e4b41eef99aa2f06e48d4e
Duc Qu <quddd812@gmail.com>
2017-10-19 17:30:18 +0700

    change db

--- src/java/DataModel/JavaConnectDB.java
+++ src/java/DataModel/JavaConnectDB.java
@@ -9,7 +9,7 @@ public class JavaConnectDB {
     public static Connection ConnectDB() throws ClassNotFoundException, SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/uf?zeroDateTimeBehavior=convertToNull", "root", "");
+            Connection con = DriverManager.getConnection("jdbc:mysql://172.30.194.36:3306/javaweb?zeroDateTimeBehavior=convertToNull", "root", "1");
             return con;
         } catch (Exception ex) {
             System.out.println("Database.getConnection() Error -->"
